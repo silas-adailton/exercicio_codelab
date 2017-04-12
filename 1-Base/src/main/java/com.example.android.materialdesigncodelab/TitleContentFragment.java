93e2e34e20919e4913e75e6fe03faa3e2d949872
Silas Oliveira <soliveira@autodoc.com.br>
2017-04-12 16:25:38 -0300

    implementando recyclerView

--- 1-Base/src/main/java/com.example.android.materialdesigncodelab/TitleContentFragment.java
+++ 1-Base/src/main/java/com.example.android.materialdesigncodelab/TitleContentFragment.java
@@ -3,16 +3,59 @@ package com.example.android.materialdesigncodelab;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class TitleContentFragment extends Fragment{
+public class TitleContentFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.item_title, null);
+
+        RecyclerView recyclerView = (RecyclerView)inflater.inflate(
+                R.layout.recycler_view,container,false);
+
+        ContentAdapter adapter = new ContentAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+
+        int titlePadding = getResources().getDimensionPixelSize(R.dimen.tile_padding);
+
+        recyclerView.setPadding(titlePadding,titlePadding,titlePadding,titlePadding);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),2));
+
+        return recyclerView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_title, parent, false));
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        public static final int LENGTH = 18;
+
+        public ContentAdapter() {
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()),parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
     }
 }
