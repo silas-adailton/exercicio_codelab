8160185133b6da647eca62b8566171844e6cfca3
Silas Oliveira <soliveira@autodoc.com.br>
2017-04-18 16:17:11 -0300

    Finalizado implementação dos fragments

--- 1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
+++ 1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
@@ -17,17 +17,15 @@
 package com.example.android.materialdesigncodelab;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.TabLayout;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Adapter;
 
 import java.util.ArrayList;
 import java.util.List;
